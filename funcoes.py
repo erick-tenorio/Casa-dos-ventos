677a06886e76eb45feeeac4b92486f3195feace8
Erick Tenorio <erick.tenorio@hotmail.com>
2024-07-25 16:11:25 -0300

    Add funcoes

---
+++ funcoes.py
@@ -0,0 +1,10 @@
+def get_regiao(uf):
+    uf_to_regiao = {
+        'AC': 'Norte', 'AL': 'Nordeste', 'AP': 'Norte', 'AM': 'Norte', 'BA': 'Nordeste',
+        'CE': 'Nordeste', 'DF': 'Centro-Oeste', 'ES': 'Sudeste', 'GO': 'Centro-Oeste',
+        'MA': 'Nordeste', 'MT': 'Centro-Oeste', 'MS': 'Centro-Oeste', 'MG': 'Sudeste',
+        'PA': 'Norte', 'PB': 'Nordeste', 'PR': 'Sul', 'PE': 'Nordeste', 'PI': 'Nordeste',
+        'RJ': 'Sudeste', 'RN': 'Nordeste', 'RS': 'Sul', 'RO': 'Norte', 'RR': 'Norte',
+        'SC': 'Sul', 'SP': 'Sudeste', 'SE': 'Nordeste', 'TO': 'Norte'
+    }
+    return uf_to_regiao.get(uf, 'Desconhecido')
